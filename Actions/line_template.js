9d04817ec54efeeaeaeb75d487722fd8d0db6e8e
tkbessler <tkbessler@gmail.com>
2024-02-07 22:31:46 -0600

    Added line update method with line object template

---
+++ Actions/line_template.js
@@ -0,0 +1,23 @@
+/** Template for Line object
+* @typedef {{
+*   id?: number
+*   type: LineType
+*   x1: number
+*   y1: number
+*   x2: number
+*   y2: number
+*   flipped?: boolean
+*   leftExtended?: boolean
+*   rightExtended?: boolean
+*   multiplier?: number
+*   layer?: number
+* }} Line
+*/
+
+/** Blue - 0 | Red - 1 | Green - 2
+* @typedef {...
+*  0 |
+*  1 |
+*  2
+* } LineType
+*/
